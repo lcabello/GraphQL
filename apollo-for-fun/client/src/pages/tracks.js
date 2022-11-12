8ae0f1c5607084d82266fa6ccd8cb9164106585c
Luis Cabello <lcabello@MacBook-Pro-de-Luis.local>
2022-11-12 22:34:04 +0100

    odyseey tutorial part 1

---
+++ apollo-for-fun/client/src/pages/tracks.js
@@ -0,0 +1,43 @@
+import React from 'react';
+import { Layout } from '../components';
+import { gql, useQuery } from '@apollo/client';
+import TrackCard from '../containers/track-card';
+import QueryResult from '../components/query-result';
+
+const TRACKS = gql`
+  query GetTrack {
+    tracksForHome {
+      id
+      title
+      author {
+        id
+        name
+        photo
+      }
+      thumbnail
+      length
+      modulesCount
+    }
+  }
+`;
+
+/**
+ * Tracks Page is the Catstronauts home page.
+ * We display a grid of tracks fetched with useQuery with the TRACKS query
+ */
+const Tracks = () => {
+
+  const { loading, error, data } = useQuery(TRACKS);
+
+  return <Layout grid>
+    <QueryResult error={error} data={data} loading={loading}>
+    {
+      data?.tracksForHome?.map((track)=>
+        <TrackCard key={track.id} track={track}></TrackCard>
+      )
+    }
+    </QueryResult>
+  </Layout>;
+};
+
+export default Tracks;
